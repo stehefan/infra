72f17c502f9641544eebde6370891e1763230116
Stefan Lier <stefan.lier@netlight.com>
2021-06-04 13:32:27 +0200

    Add cloudfront distribution and certificates

--- home.tf
+++ home.tf
@@ -6,8 +6,8 @@ resource "aws_route53_zone" "website_zones" {
 resource "aws_route53_record" "website_records" {
   for_each = toset(local.website_urls)
   zone_id = aws_route53_zone.website_zones[each.key].zone_id
-  name    = each.key
-  type    = "A"
+  name = each.key
+  type = "A"
 
   alias {
     name = aws_cloudfront_distribution.s3_distribution.domain_name
@@ -16,22 +16,33 @@ resource "aws_route53_record" "website_records" {
   }
 }
 
+resource "aws_route53_record" "website_www_records" {
+  for_each = toset(local.website_urls)
+  zone_id = aws_route53_zone.website_zones[each.key].zone_id
+  name = "www.${each.key}"
+  type = "CNAME"
+  ttl = 60
+  records = [
+    each.key]
+}
+
 resource "aws_route53_record" "validation_records" {
   for_each = {
-    for dvo in aws_acm_certificate.website_certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-      zone_id = dvo.domain_name == "stehefan.de" ? aws_route53_zone.website_zones["stehefan.de"].zone_id : aws_route53_zone.website_zones["stefanlier.de"].zone_id
-    }
+  for dvo in aws_acm_certificate.website_certificate.domain_validation_options : dvo.domain_name => {
+    name = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type = dvo.resource_record_type
+    zone_id = dvo.domain_name == "stehefan.de" ? aws_route53_zone.website_zones["stehefan.de"].zone_id : aws_route53_zone.website_zones["stefanlier.de"].zone_id
+  }
   }
 
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = each.value.zone_id
+  name = each.value.name
+  records = [
+    each.value.record]
+  ttl = 60
+  type = each.value.type
+  zone_id = each.value.zone_id
 }
 
 resource "aws_s3_bucket" "website_bucket" {
@@ -45,12 +56,15 @@ resource "aws_cloudfront_origin_access_identity" "website_oai" {
 
 data "aws_iam_policy_document" "s3_policy" {
   statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.website_bucket.arn}/*"]
+    actions = [
+      "s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.website_bucket.arn}/*"]
 
     principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.website_oai.iam_arn]
+      type = "AWS"
+      identifiers = [
+        aws_cloudfront_origin_access_identity.website_oai.iam_arn]
     }
   }
 }
@@ -64,7 +78,11 @@ resource "aws_acm_certificate" "website_certificate" {
   provider = aws.us-east-1
 
   domain_name = local.website_urls[0]
-  subject_alternative_names = [local.website_urls[1]]
+  subject_alternative_names = [
+    local.website_urls[1],
+    "*.${local.website_urls[0]}",
+    "*.${local.website_urls[1]}",
+  ]
   validation_method = "DNS"
 
   lifecycle {
@@ -75,20 +93,20 @@ resource "aws_acm_certificate" "website_certificate" {
 resource "aws_acm_certificate_validation" "website_certificate_validation" {
   provider = aws.us-east-1
 
-  certificate_arn         = aws_acm_certificate.website_certificate.arn
+  certificate_arn = aws_acm_certificate.website_certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Cloudfront distribution for ${join(" and ", local.website_urls)}"
+  enabled = true
+  is_ipv6_enabled = true
+  comment = "Cloudfront distribution for ${join(" and ", local.website_urls)}"
   default_root_object = "index.html"
-  aliases = local.website_urls
+  aliases = concat(local.website_urls, formatlist("www.%s", local.website_urls))
 
   origin {
     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
+    origin_id = local.s3_origin_id
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.website_oai.cloudfront_access_identity_path
@@ -96,14 +114,19 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
+    allowed_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS"]
+    cached_methods = [
+      "GET",
+      "HEAD"]
     target_origin_id = local.s3_origin_id
     compress = true
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
 
     forwarded_values {
       query_string = false
